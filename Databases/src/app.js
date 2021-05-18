d5658ab0a66dfbaa9cbcd07f81076463ff5fb1bd
Sireesh reddy <koduru.reddy.19cse@bmu.edu.in>
2021-05-18 08:53:08 +0530

    second update

--- Databases/src/app.js
+++ Databases/src/app.js
@@ -14,5 +14,27 @@ app.get("/",(req, res) => {
 // res.send("hello testin41356g");
 res.render("index"); 
 }); 
+app.get("/signup",(req, res) => {
+res.render("signup");
+})
+app.post("/signup",async(req, res) => {
+    try{
+    const userrecord=new register({
+name:req.body.name,
+country:req.body.country,
+city:req.body.city,
+state:req.body.state,
+zip:req.body.zip,
+username:req.body.username,
+password:req.body.password
+    })
+    const signupstatus=userrecord.save();
+    res.render("index");
+}
+catch(e){
+res.send(e);
+res.status(404);
+}
 
+})
 app.listen(3000,() => {"server is listening to 3000 port"});
--- Databases/src/models/regdetails.js
+++ Databases/src/models/regdetails.js
@@ -1,1 +1,17 @@
 const mongoose= require('mongoose');
+const usersschema= new mongoose.Schema(
+    {
+name: {type:String,required:true},
+country: {type:String,required:true},
+city:{type:String,required:true},
+state:{type:String,required:true},
+zip:{type:String,required:true},
+username:{type:String,required:true},
+password:{type:String,required:true},
+    }
+)
+
+
+
+const register=new mongoose.model('userdata',usersschema);
+module.exports =register;
